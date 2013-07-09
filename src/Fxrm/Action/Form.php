6e9c033e9492c4538f8f4dd697bc5c09f7316656
Nick Matantsev <nick@sigmaxis.com>
2013-07-09 12:30:44 +0000

    Refactored service and form implementations

--- src/Fxrm/Action/Form.php
+++ src/Fxrm/Action/Form.php
@@ -8,8 +8,8 @@
 namespace Fxrm\Action;
 
 /**
- * Form renderer for model methods.
- * Non-AJAX (redirect) mode is supported for reporting results.
+ * Service form implementation that uses POST parameters to encode arguments and JSON for results.
+ * Any service implementation may be passed in.
  */
 class Form {
     private $serializer;
@@ -21,13 +21,151 @@ class Form {
     private $fieldValues;
     private $returnValue, $fieldError, $actionError, $hasReturnValue;
 
-    function __construct(ContextSerializer $serializer, $formSignature, $endpointUrl, $className, $methodName) {
-        $this->serializer = $serializer;
+    public static function setupErrorHandler() {
+        // error -> exception converter
+        set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+            // ignore errors when @ operator is used
+            if ( ! error_reporting()) {
+                return false;
+            }
 
-        $classInfo = new \ReflectionClass($className);
+            throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
+        });
+    }
+
+    public static function invoke(Service $service, $methodName) {
+        // check for GPC slashes kid-gloves
+        if (get_magic_quotes_gpc()) {
+            throw new \Exception('magic_quotes_gpc mode must not be enabled');
+        }
+
+        $bodyFunctionInfo = new \ReflectionMethod($service->getClassName(), $methodName);
+
+        // collect necessary parameter data
+        $apiParameterList = array();
+
+        // public request values are saved raw, before deserialization (the latter may fail)
+        $publicRequestValues = (object)array();
+        $fieldErrors = (object)array();
+
+        foreach ($bodyFunctionInfo->getParameters() as $bodyFunctionParameter) {
+            $param = $bodyFunctionParameter->getName();
+            $class = $bodyFunctionParameter->getClass();
+
+            $value = null;
+
+            // try corresponding parameter name, or append a $ for private (e.g. password) fields
+            if (isset($_POST[$param])) {
+                $value = $_POST[$param];
+
+                // save public values to be sent back as necessary
+                $publicRequestValues->$param = $value;
+            } elseif (isset($_POST["$param\$"])) {
+                $value = $_POST["$param\$"];
+
+                // not sending back private values
+                $publicRequestValues->$param = null;
+            }
+
+            try {
+                $apiParameterList[] = $service->getSerializer()->import($class->getName(), $value);
+            } catch(\Exception $e) {
+                $fieldErrors->$param = $service->getSerializer()->exportException($e);
+            }
+        }
+
+        // report field validation errors
+        if (count((array)$fieldErrors) > 0) {
+            // using dedicated 400 status (bad client request syntax)
+            self::report($publicRequestValues, 400, json_encode($fieldErrors));
+            return;
+        }
+
+        // catch any output
+        ob_start();
+
+        try {
+            $instance = $service->createInstance();
+        } catch(\Exception $e) {
+            ob_end_clean();
+
+            // report exception
+            // using dedicated 400 status (bad client request syntax)
+            self::report($publicRequestValues, 400, json_encode($service->getSerializer()->exportException($e)));
+            return;
+        }
+
+        try {
+            $result = $bodyFunctionInfo->invokeArgs($instance, $apiParameterList);
+
+            if (ob_get_length() > 0) {
+                throw new \Exception('unexpected output');
+            }
+        } catch(\Exception $e) {
+            ob_end_clean();
+
+            // report exception
+            // using dedicated 500 status (syntax was OK but server-side error)
+            self::report($publicRequestValues, 500, json_encode($service->getSerializer()->exportException($e)));
+            return;
+        }
+
+        ob_end_clean();
+
+        // result output
+        header('Content-Type: text/json');
+
+        self::report($publicRequestValues, 200, $service->getSerializer()->export($result));
+    }
+
+    private static function report($fieldValues, $httpStatus, $jsonData) {
+        // non-AJAX mode
+        if (isset($_GET['redirect']) && isset($_SERVER['HTTP_REFERER'])) {
+            // identify which form on the originating page this is intended for
+            $formSignature = $_GET['redirect'];
+
+            // work with referer URL query-string
+            $urlParts = explode('?', $_SERVER['HTTP_REFERER'], 2);
+
+            $query = count($urlParts) > 1 ? $urlParts[1] : '';
+
+            // remove old payload
+            $queryParts = $query === '' ? array() : array_filter(explode('&', $query), function ($q) {
+                return substr($q, 0, 3) !== '$_=';
+            });
+
+            $queryParts[] = '$_=' . base64_encode(join("\x00", array($formSignature, json_encode($fieldValues), $httpStatus, $jsonData)));
+
+            // using the dedicated 303 response type
+            header('HTTP/1.1 303 See Other');
+            header('Location: ' . $urlParts[0] . '?' . join('&', $queryParts));
+            return;
+        }
+
+        // AJAX mode
+        $statusLabels = array(
+            200 => 'Success',
+            400 => 'Bad Syntax',
+            500 => 'Internal Error'
+        );
+
+        header('HTTP/1.1 ' . $httpStatus . ' ' . $statusLabels[$httpStatus]);
+        header('Content-Type: text/json');
+        echo $jsonData;
+    }
+
+    function __construct(Service $service, $baseUrl, $paramMap, $methodName, $formDifferentiator = null) {
+        $this->serializer = $service->getSerializer();
+
+        $classInfo = new \ReflectionClass($service->getClassName());
         $methodInfo = $classInfo->getMethod($methodName);
 
-        $this->url = $endpointUrl === null ? $endpointUrl : $this->addUrlRedirectHash($endpointUrl, $formSignature);
+        $serviceUrl = $service->generateUrl($baseUrl, $paramMap);
+
+        // basing form signature on service URL, since it is unique to this service instance by definition
+        $formSignature = md5(json_encode(array($serviceUrl, $methodName, $formDifferentiator)));
+
+        $this->url = $this->addUrlRedirectHash($serviceUrl, $formSignature);
         $this->paramTypes = (object)array();
 
         foreach ($methodInfo->getParameters() as $param) {
--- src/Fxrm/Action/Service.php
+++ src/Fxrm/Action/Service.php
@@ -8,9 +8,7 @@
 namespace Fxrm\Action;
 
 /**
- * Form handler for model methods.
- * This variant converts PHP request parameters into function arguments and outputs the result as JSON.
- * Non-AJAX (redirect) mode is supported for reporting results.
+ * Service implementation that uses HTTP GET query parameters to construct an instance.
  */
 class Service {
     private $serializer;
@@ -35,113 +33,30 @@ class Service {
         }
     }
 
-    public function createForm($baseUrl, $paramMap, $methodName, $formDifferentiator = null) {
+    public function getSerializer() {
+        return $this->serializer;
+    }
+
+    public function getClassName() {
+        return $this->className;
+    }
+
+    public function generateUrl($baseUrl, $paramMap) {
         $rawParamMap = array();
 
         foreach ($this->argumentMap as $name => $className) {
             $rawParamMap[$name] = $this->serializer->export($paramMap[$name]);
         }
 
-        $formSignature = array(
-            $this->className,
-            $rawParamMap,
-            $methodName,
-            $formDifferentiator
-        );
-
-        $instanceEndpointUrl = $this->addUrlParams($baseUrl, $rawParamMap);
-
-        return new Form($this->serializer, md5(json_encode($formSignature)), $instanceEndpointUrl, $this->className, $methodName);
+        return $this->addUrlParams($baseUrl, $rawParamMap);
     }
 
-    public function invoke($methodName) {
+    public function createInstance() {
         // check for GPC slashes kid-gloves
         if (get_magic_quotes_gpc()) {
             throw new \Exception('magic_quotes_gpc mode must not be enabled');
         }
 
-        $bodyFunctionInfo = new \ReflectionMethod($this->className, $methodName);
-
-        // collect necessary parameter data
-        $apiParameterList = array();
-
-        // public request values are saved raw, before deserialization (the latter may fail)
-        $publicRequestValues = (object)array();
-        $fieldErrors = (object)array();
-
-        foreach ($bodyFunctionInfo->getParameters() as $bodyFunctionParameter) {
-            $param = $bodyFunctionParameter->getName();
-            $class = $bodyFunctionParameter->getClass();
-
-            $value = null;
-
-            // try corresponding parameter name, or append a $ for private (e.g. password) fields
-            if (isset($_POST[$param])) {
-                $value = $_POST[$param];
-
-                // save public values to be sent back as necessary
-                $publicRequestValues->$param = $value;
-            } elseif (isset($_POST["$param\$"])) {
-                $value = $_POST["$param\$"];
-
-                // not sending back private values
-                $publicRequestValues->$param = null;
-            }
-
-            try {
-                $apiParameterList[] = $this->serializer->import($class->getName(), $value);
-            } catch(\Exception $e) {
-                $fieldErrors->$param = $this->serializer->exportException($e);
-            }
-        }
-
-        // report field validation errors
-        if (count((array)$fieldErrors) > 0) {
-            // using dedicated 400 status (bad client request syntax)
-            $this->report($publicRequestValues, 400, json_encode($fieldErrors));
-            return;
-        }
-
-        // catch any output
-        ob_start();
-
-        try {
-            $instance = $this->createInstance();
-        } catch(\Exception $e) {
-            ob_end_clean();
-
-            // report exception
-            // using dedicated 400 status (bad client request syntax)
-            $this->report($publicRequestValues, 400, json_encode($this->exportException($e)));
-            return;
-        }
-
-        try {
-            $result = $bodyFunctionInfo->invokeArgs($instance, $apiParameterList);
-
-            if (ob_get_length() > 0) {
-                throw new \Exception('unexpected output');
-            }
-        } catch(\Exception $e) {
-            ob_end_clean();
-
-            // report exception
-            // using dedicated 500 status (syntax was OK but server-side error)
-            $this->report($publicRequestValues, 500, json_encode($this->exportException($e)));
-            return;
-        }
-
-        ob_end_clean();
-
-        // result output
-        header('Content-Type: text/json');
-
-        $output = array();
-        $this->jsonPrint($result, $output);
-        $this->report($publicRequestValues, 200, join('', $output));
-    }
-
-    public function createInstance() {
         // collect arguments
         // @todo deal with these exceptions gracefully? shouldn't it be a 404 though
         $argumentList = array();
@@ -155,80 +70,6 @@ class Service {
         return $this->serializer->constructArgs($this->className, $argumentList);
     }
 
-    private function report($fieldValues, $httpStatus, $jsonData) {
-        // non-AJAX mode
-        if (isset($_GET['redirect']) && isset($_SERVER['HTTP_REFERER'])) {
-            // identify which form on the originating page this is intended for
-            $formSignature = $_GET['redirect'];
-
-            // work with referer URL query-string
-            $urlParts = explode('?', $_SERVER['HTTP_REFERER'], 2);
-
-            $query = count($urlParts) > 1 ? $urlParts[1] : '';
-
-            // remove old payload
-            $queryParts = $query === '' ? array() : array_filter(explode('&', $query), function ($q) {
-                return substr($q, 0, 3) !== '$_=';
-            });
-
-            $queryParts[] = '$_=' . base64_encode(join("\x00", array($formSignature, json_encode($fieldValues), $httpStatus, $jsonData)));
-
-            // using the dedicated 303 response type
-            header('HTTP/1.1 303 See Other');
-            header('Location: ' . $urlParts[0] . '?' . join('&', $queryParts));
-            return;
-        }
-
-        // AJAX mode
-        $statusLabels = array(
-            200 => 'Success',
-            400 => 'Bad Syntax',
-            500 => 'Internal Error'
-        );
-
-        header('HTTP/1.1 ' . $httpStatus . ' ' . $statusLabels[$httpStatus]);
-        header('Content-Type: text/json');
-        echo $jsonData;
-    }
-
-    private function jsonPrint($result, &$output) {
-        if (get_class($result) === 'stdClass') {
-            // anonymous object
-            $first = true;
-
-            $output[] = '{';
-            foreach ($result as $k => $v) {
-                if ($first) {
-                    $first = false;
-                } else {
-                    $output[] = ',';
-                }
-
-                $output[] = json_encode($k);
-                $output[] = ':';
-                $this->jsonPrint($v, $output);
-            }
-            $output[] = '}';
-        } elseif (is_array($result)) {
-            $first = true;
-
-            $output[] = '[';
-            foreach ($result as $v) {
-                if ($first) {
-                    $first = false;
-                } else {
-                    $output[] = ',';
-                }
-
-                $this->jsonPrint($v, $output);
-            }
-            $output[] = ']';
-        } else {
-            // pipe everything else through the exporter
-            $output[] = json_encode($this->serializer->export($result));
-        }
-    }
-
     private function addUrlParams($url, $rawParamMap) {
         $urlParts = explode('?', $url, 2);
 
@@ -236,18 +77,6 @@ class Service {
         $query = (count($urlParts) === 2 ? $urlParts[1] . '&' : '') . http_build_query($rawParamMap);
 
         return $baseUrl . '?' . $query;
-    }
-
-    public static function setupErrorHandler() {
-        // error -> exception converter
-        set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-            // ignore errors when @ operator is used
-            if ( ! error_reporting()) {
-                return false;
-            }
-
-            throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
-        });
     }
 }
 
