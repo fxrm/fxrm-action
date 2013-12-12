01434de9c304d5fa4421d41bd997783ff1c12a32
Nick Matantsev <nick@sigmaxis.com>
2013-12-11 22:10:51 -0500

    Added Unix time conversion

---
+++ src/Fxrm/Action/UnixTimeSerializer.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Nick Matantsev <nick.matantsev@gmail.com>
+ * @copyright Copyright (c) 2013, Nick Matantsev
+ */
+
+namespace Fxrm\Action;
+
+class UnixTimeSerializer implements Serializer {
+    function export($ctx, $object) {
+        if ($object instanceof \DateTime) {
+            return $object->getTimestamp();
+        }
+
+        throw new \Exception('expecting DateTime');
+    }
+
+    function import($ctx, $class, $value) {
+        if ($class !== 'DateTime') {
+            throw new \Exception('must expect DateTime');
+        }
+
+        return new \DateTime('@' . (int)$value);
+    }
+}
+
+?>
